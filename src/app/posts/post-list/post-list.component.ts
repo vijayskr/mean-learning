582cb3f58da066f042645d597b8b1a65e282c133
Vijayasankar Balasubramanian <vijayasankar@outlook.com>
2018-07-05 20:27:11 -0500

    TillClickEvent

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  // posts = [
+  //   {title: 'First Post', content: 'This is first post'},
+  //   {title: 'Second Post', content: 'This is second post'},
+  //   {title: 'Third Post', content: 'This is third post'}
+  // ];
+  @Input() posts = [];
+
+}
